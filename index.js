86ff7916a966037c187bd6717aed7cc5a00fdd86
samrakhan23 <samrakhalil93@gmail.com>
2024-03-22 03:02:22 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+//                                SIMPLE CALCULATOR
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter First number", type: "number", name: "firstNumber" },
+    { message: "Enter Second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Division", "Multiplication"],
+    },
+]);
+if (answer.operator == "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator == "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator == "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator == "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else {
+    console.log("Please Enter Valid Operator");
+}
